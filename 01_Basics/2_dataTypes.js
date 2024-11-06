6ea0e27e243b200adc2dc0c783b165ef2b43c60a
Prashant Satyavan Jagtap <prashantjagtap2130@gmail.com>
2024-11-06 16:07:04 +0530

    Data types in javascript

---
+++ 01_Basics/2_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version
+
+alert("Hello World"); //we are using nodejs not browser
+
+
+
+//*datatypes*
+//number
+//string
+//boolean
+//null => is a standalone value (typeof null is object)
+//undefined // abhi value assign nahi hui hai (typeof undefined is undefined)
+//symbol
+//bigInt
+
+//object 
